de274c5cec72c64a1ee9ce1f512d51022a5fa3b1
Henrik Ygge <henrik@meepo.se>
2020-12-23 19:23:38 +0100

    solved day 16

--- src/main/java/Day16.java
+++ src/main/java/Day16.java
@@ -4,10 +4,42 @@ public class Day16 {
 
     public static void main(String[] args) {
         String code = Util.readString();
+        //String code = "03036732577212944063491565474664";
+        String part1 = part1(code);
+        System.out.println("part 1: " + part1.substring(0, 8));
+
+        String part2 = part2(code);
+        System.out.println("part 2: " + part2);
+    }
+
+    private static String part2(String code) {
+        int offset = Integer.parseInt(code.substring(0, 7));
+        System.out.println(offset);
+        int len = code.length();
+        int[] data = new int[len*10000];
+        for (int i = 0; i < len; ++i) {
+            int v = code.charAt(i)-'0';
+            for (int j = 0; j < 10000; ++j) {
+                data[len*j+i] = v;
+            }
+        }
+        for (int i = 0; i < 100; ++i) {
+            for (int j = data.length-2; j >= offset; --j) {
+                data[j] = Math.abs(data[j]+data[j+1])%10;
+            }
+        }
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < 8; ++i) {
+            res.append(data[offset+i]);
+        }
+        return res.toString();
+    }
+
+    private static String part1(String code) {
         for (int i = 0; i < 100; ++i) {
             code = runFFT(code);
         }
-        System.out.println("part 1: " + code.substring(0, 8));
+        return code;
     }
 
     private static String runFFT(String code) {
